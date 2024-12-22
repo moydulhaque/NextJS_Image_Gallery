6609346dc04756178a55066219390d36462b2831
Hendrik Liebau <mail@hendrik-liebau.de>
2024-12-22 20:06:47 +0100

    Add debug logs

--- test/lib/create-next-install.js
+++ test/lib/create-next-install.js
@@ -23,6 +23,12 @@ async function installDependencies(cwd, tmpDir) {
     args.push('--prefer-offline')
   }
 
+  await execa('pnpm', ['--version'], {
+    cwd,
+    stdio: ['ignore', 'inherit', 'inherit'],
+    env: process.env,
+  })
+
   await execa('pnpm', args, {
     cwd,
     stdio: ['ignore', 'inherit', 'inherit'],
@@ -129,6 +135,8 @@ async function createNextInstall({
           return prev
         }, {}),
       }
+
+      console.log('combinedDependencies', combinedDependencies)
 
       const scripts = {
         debug: `NEXT_PRIVATE_SKIP_CANARY_CHECK=1 NEXT_TELEMETRY_DISABLED=1 NEXT_TEST_NATIVE_DIR=${process.env.NEXT_TEST_NATIVE_DIR} node --inspect --trace-deprecation --enable-source-maps node_modules/next/dist/bin/next`,
